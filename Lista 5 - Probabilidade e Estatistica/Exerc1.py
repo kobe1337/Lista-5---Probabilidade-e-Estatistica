55c1d58b6a737b4bda9a53e4363576443a2488a3
kobe1337 <viniciusmota1337@gmail.com>
2020-11-25 06:54:30 +0000

    Initial commit

---
+++ Lista 5 - Probabilidade e Estatistica/Exerc1.py
@@ -0,0 +1,8 @@
+# Disciplina: Probabilidade e Estatística
+# Aluno:
+# Lista 5
+
+l_con = ["10","13","17","9","8","11","13","7"]
+
+soma =+ sum(l_con)
+print(soma)
---
+++ main.py
@@ -0,0 +1,11 @@
+# Disciplina: Probabilidade e Estatística
+# Aluno:
+# Lista 5
+
+l_con = [10,13,17,9,8,11,13,7]
+
+soma = sum(l_con)
+tam = len(l_con)
+md_arit = soma/tam
+print(soma,tam)
+print("Média Aritmética Simples: ",md_arit)
